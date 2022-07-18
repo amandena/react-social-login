c49ed996d0d78aafd2f9d31e88b8820e7360dfb5
amandena <manda.m.johnson@gmail.com>
2022-07-18 17:32:15 -0400

    imported components into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import Post from './pages/Post'
 
 function App() {
   return (
     <div className='App'>
-      App
+      <Navbar />
+      <Post />
     </div>
   )
 }
