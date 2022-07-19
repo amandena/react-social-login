a4a08431eea80daae116bfb630ed45dd1b8bc04c
amandena <manda.m.johnson@gmail.com>
2022-07-19 16:06:58 -0400

    added logout handler, check for user and updated display with user data

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom"
 
 const Navbar = ({ user }) => {
+  const logout = () => {
+    window.open("http://localhost:5001/auth/logout", "_self")
+  }
+
+  if (!user) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="navbar">
       <span className="logo"><Link className="link" to="/">Social App</Link></span>
       {user ? (
         <ul className="list">
           <li className="listItem">
-            <img src="https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg" alt="" className="avatar" />
+            <img src={user.photos[0].value} alt="" className="avatar" />
           </li>
-          <li className="listItem">John Doe</li>
-          <li className="listItem">Logout</li>
+          <li className="listItem">{user.displayName}</li>
+          <li className="listItem" onClick={logout}>Logout</li>
         </ul>
       ) : (
         <Link className="link" to="/login">Login</Link>
