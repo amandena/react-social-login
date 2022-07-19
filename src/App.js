d5033fce8be9c762af48095aee5f6654b07adca2
amandena <manda.m.johnson@gmail.com>
2022-07-19 13:26:15 -0400

    added routes and dummy user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
-// import Post from './pages/Post'
-// import Home from './pages/Home'
+import Post from './pages/Post'
+import Home from './pages/Home'
 
 function App() {
+  const user = true
+
   return (
-    <div className='App'>
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar user={user} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+          <Route path='/post/:id' element={user ? <Post /> : <Navigate to='/login' />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
