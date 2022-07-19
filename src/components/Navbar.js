ee84051272efc7934d223ca57607d003beae257c
amandena <manda.m.johnson@gmail.com>
2022-07-19 13:30:36 -0400

    passed user prop to conditionally render login/logout buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <div className="navbar">
-      <span className="logo">Social App</span>
-      <ul className="list">
-        <li className="listItem">
-          <img src="https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg" alt="" className="avatar" />
-        </li>
-        <li className="listItem">John Doe</li>
-        <li className="listItem">Logout</li>
-      </ul>
+      <span className="logo"><Link className="link" to="/">Social App</Link></span>
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img src="https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg" alt="" className="avatar" />
+          </li>
+          <li className="listItem">John Doe</li>
+          <li className="listItem">Logout</li>
+        </ul>
+      ) : (
+        <Link className="link" to="/login">Login</Link>
+      )}
     </div>
   )
 }
